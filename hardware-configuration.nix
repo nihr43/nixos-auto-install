e636d3ea14d790d69f7168debff96493a945c6ab
Kevin Cox <kevincox@kevincox.ca>
2022-06-27 11:26:48 -0400

    Improve hardware support.

--- hardware-configuration.nix
+++ hardware-configuration.nix
@@ -3,15 +3,32 @@
 
 	boot.loader.systemd-boot.enable = true;
 
+	boot.initrd.kernelModules = [
+		"kvm-intel"
+		"virtio_balloon"
+		"virtio_console"
+		"virtio_rng"
+	];
+
+	boot.initrd.availableKernelModules = [
+		"9p"
+		"9pnet_virtio"
+		"ata_piix"
+		"nvme"
+		"sr_mod"
+		"uhci_hcd"
+		"virtio_blk"
+		"virtio_mmio"
+		"virtio_net"
+		"virtio_pci"
+		"virtio_scsi"
+		"xhci_pci"
+	];
+
 	fileSystems."/boot" = {
 		device = "/dev/disk/by-label/boot";
 		fsType = "vfat";
 	};
-
-	boot.initrd.availableKernelModules = [
-		"nvme"
-		"xhci_pci"
-	];
 
 	boot.initrd.luks.devices.root = {
 		device = "/dev/disk/by-label/root";
